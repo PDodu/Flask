ad08aad43daf194927d414db24273c62696b8f84
PDodu <52000806+PDodu@users.noreply.github.com>
2019-09-25 20:15:08 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__, static_url_path='', static_folder='templates/static')
 
@@ -13,7 +13,7 @@ posts = [
     },
 
       {
-        'author': 'Jane Doe',
+        'author': 'Peter Dodu',
         'title': 'Blog Post 2',
         'Content': 'Second post content',
         'date_posted': 'September 20, 2019'   
@@ -35,12 +35,21 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))    
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
+    
 if __name__=='__main__':
     app.run(debug=True)
